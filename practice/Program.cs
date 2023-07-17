4e2e9d66887d8732136b5de9b71f98e906de27f8
mahmud200405 <mahmudjonb515@gmail.com>
2023-07-17 23:43:38 +0500

    first commit

---
+++ practice/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+//-----------------------------OBJECTE------------------------------//
+
+var pos =new Post()
+{
+    Title = "Softclub",
+    Deskription = "Free Lesson",
+    Data = new DateTime(2023, 07, 17)
+    
+};
+System.Console.WriteLine(pos.Title);
+System.Console.WriteLine(pos.Deskription);
+System.Console.WriteLine(pos.Data);
+pos.Publish();
+pos.Like(1);
+System.Console.WriteLine($"Like: {pos.Like(1)}");
+System.Console.Write($"Comment : ");
+string comment=Console.ReadLine();
+pos.Comment(comment);
+
+
+//------------------------------CLASS------------------------------//
+
+
+   public class  Post{
+     public string Title { get; set; }
+     public string Deskription { get; set; }
+     public DateTime Data;
+
+
+     public void Publish(){
+System.Console.WriteLine($"Text is Publish");
+    }
+    public int Like(int n){
+        n++;
+     return n;
+    }
+    public string Comment(string comment){
+        return comment;
+    }
+    
+
+   }
+
+
+   
